bb1184d2abd45a1aebd3433bf86831208fce4046
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-03-16 15:10:19 -0500

    server initial set up

---
+++ butterfly-backend/controllers/butterfly.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const butterfly = express.Router()
+//==========presentational route=========//
+
+//==========presentational route end=========//
+
+//==========functional route=========//
+
+//==========functional route end=========//
---
+++ butterfly-backend/server.js
@@ -0,0 +1,40 @@
+//======dependencies===========//
+const express = require('express')
+const mongoose = require('mongoose')
+// const cors = require('cors')
+PORT = 3003
+//=======config=============//
+const app = express()
+mongoose.connect('mongodb://localhost:27017/butterfly', {useNewUrlParser:true})
+mongoose.connection.once('open', () => {
+  console.log('connect to mongoose...');
+})
+//=======mongoose=============//
+mongoose.connection.on('error', () => {
+  console.log(err.message + 'is Mongod not running?');
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('mongo disconnected');
+})
+//=======middleware=============//
+app.use(express.json())
+// const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+// app.use(cors(corsOptions))
+//=======controllers=============//
+
+//=======listener=============//
+app.get('/', (req, res) => {
+  res.send('hi')
+})
+app.listen(PORT, () => {
+  console.log('serving is listening : ', PORT);
+})
