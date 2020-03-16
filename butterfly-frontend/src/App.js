57c8f147a8243d154c1046e74c2bbfe54fac62b2
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-03-16 17:08:53 -0500

    began frontend

--- butterfly-frontend/src/App.js
+++ butterfly-frontend/src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+/*
+********************************************************
+          Define
+********************************************************
+*/
+let baseURL = process.env.REACT_APP_BASEURL
+
+if(process.env.NODE_ENV === 'development'){
+  baseURL = 'http:// localhost:3003'
+}
+
+// else {
+//   
+//  baseURL = ''
+// }
+
+
+
+
+
+
+/*
+********************************************************
+          Begin Class
+********************************************************
+*/
+
+
+
+class App extends React.Component {
+
+
+
+
+
+/*
+  ********************************************************
+        WAITS FOR BROWSER BEFORE GRABS INFO FROM SERVER
+    ********************************************************
+    */
+
+
+
+
+
+    /*
+     ********************************************************
+                GRABS POST FROM SERVER
+     ********************************************************
+     */
+
+
+
+
+
+
+     /*
+       ********************************************************
+                   ADDS NEW POST
+       ********************************************************
+       */
+
+
+
+
+
+       /*
+       ********************************************************
+                 GET POSTS FOR SHOW PAGE
+       ********************************************************
+       */
+
+
+
+
+       /*
+     ********************************************************
+               Delete POSTS
+     ********************************************************
+     */
+
+
+
+
+
+     /*
+     ********************************************************
+                UPDATE RECIPES
+     ********************************************************
+     */
+
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi</h1>
     </div>
-  );
+  )}
 }
 
 export default App;
