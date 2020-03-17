528ab487536a0f4878147113c14289c3e2f08682
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-03-17 15:39:12 -0500

    conditional rendering for image and videos. Began toggleLike function

--- butterfly-frontend/src/App.js
+++ butterfly-frontend/src/App.js
@@ -45,6 +45,7 @@ class App extends React.Component {
     this.getPosts = this.getPosts.bind(this)
     this.handleUpdatePost = this.handleUpdatePost.bind(this)
     this.handleSignin = this.handleSignin.bind(this)
+    this.toggleLikes = this.toggleLikes.bind(this)
     // this.getCurrentUser = this.getCurrentUser.bind(this)
   }
 
@@ -147,6 +148,48 @@ class App extends React.Component {
        }
      }
 
+
+
+     /*
+    ********************************************************
+            Update likes POSTS
+    ********************************************************
+    */
+
+    async toggleLikes (post){
+         console.log(post)
+         try {
+           let response = await fetch( `${baseURL}/butterfly/${post._id}`, {
+             method: 'PUT',
+             body: JSON.stringify({likes: post.likes + 1}),
+             headers:{
+               'Content-type': 'application/json'
+             }
+           })
+
+
+           let updatedPost =  await response.json()
+
+           console.log(updatedPost)
+
+           const foundPost = this.state.posts.findIndex(postFound=>
+             postFound._id === post._id
+           )
+           console.log(foundPost);
+
+         const copyPosts = [...this.state.posts]
+console.log(copyPosts);
+         copyPosts[foundPost].likes = updatedPost.likes
+
+         console.log(updatedPost);
+         this.setState({
+           post: copyPosts
+         })
+
+       }catch(e){
+         console.error(e)
+       }
+       }
        /*
      ********************************************************
                Delete POSTS
@@ -206,7 +249,16 @@ class App extends React.Component {
                   <button onClick={() => {
                     this.deletePost(post._id)
                   }}>delete</button>
-                  <Post post={post} handleUpdatePost={this.handleUpdatePost}/>
+
+
+<Post post={post} handleUpdatePost={this.handleUpdatePost}/>
+                <div onClick={() => {
+                    this.toggleLikes(post)
+                  }}>{
+                    post.likes? '❤️': '🤍'
+                  }</div>
+
+
                 </div>
               )}
             )}
--- butterfly-frontend/src/components/Show.js
+++ butterfly-frontend/src/components/Show.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 class Show extends React.Component {
   render () {
+
     return (
       <div>
         <div className="modal">
          <h3>{this.props.post.title.toUpperCase()}</h3>
          <hr/>
-         <img href={this.props.post.image}></img>
-         <iframe width="560" height="315" src={this.props.post.video} frameBorder="0"></iframe>
+         {
+           this.props.post.image? <img src={this.props.post.image}></img> : ''
+         }
+
+         {
+           this.props.post.video? <iframe width="560" height="315" src={this.props.post.video} frameBorder="0"></iframe>: ''
+         }
          <h4>{this.props.post.caption}</h4>
          <h6><span>Likes:</span> {this.props.post.likes}</h6>
          {/* <p><span>comments:</span> { this.props.holiday.description } </p> */}
