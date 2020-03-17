5007a480192ffcd6c2738ab3a2ff12da0ba85155
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-03-17 10:22:33 -0500

    fixed update

--- butterfly-frontend/src/components/Update.js
+++ butterfly-frontend/src/components/Update.js
@@ -11,6 +11,19 @@ class Update extends Component{
     }
     this.handleChange = this.handleChange.bind(this)
   }
+
+  componentDidMount(){
+    if (this.props.post) {
+      this.setState({
+        title: this.props.post.title,
+        image: this.props.post.image,
+        video: this.props.post.video,
+        caption:this.props.post.caption,
+        _id: this.props.post._id
+      })
+    }
+  }
+
   handleChange(event){
     this.setState({
       [event.currentTarget.id]:event.currentTarget.value
@@ -22,23 +35,22 @@ class Update extends Component{
         <h1>Update Form</h1>
         <form onSubmit={
           (event) => {
-
             this.props.handleUpdatePost(event,this.state)
             this.props.toggleForm()
           }
         }>
 
           <label htmlFor="title">title</label>
-          <input type="text" id="title" name="title"  onChange={this.handleChange}  value={this.props.post.title}/>
+          <input type="text" id="title" name="title"  onChange={this.handleChange}  value={this.state.title}/>
 
           <label htmlFor="image">image</label>
-          <input type="text" id="image" name="image" onChange={this.handleChange} value={this.props.post.image} />
+          <input type="text" id="image" name="image" onChange={this.handleChange} value={this.state.image} />
 
           <label htmlFor="video">video</label>
-          <input type="text" id="video" name="video" onChange={this.handleChange} value={this.props.post.video} placeholder="upload your video"/>
+          <input type="text" id="video" name="video" onChange={this.handleChange} value={this.state.video} placeholder="upload your video"/>
 
           <label htmlFor="caption">caption</label>
-          <input type="text" id="caption" name="caption" onChange={this.handleChange} value={this.props.post.caption} placeholder="put your caption"/>
+          <input type="text" id="caption" name="caption" onChange={this.handleChange} value={this.state.caption} placeholder="put your caption"/>
 
           <input type="submit" value="submit"/>
         </form>
