04c58a8009feee06380561f5a3cf676ba3b3c95e
Safia Hirsi <safiahirsi@Safias-MacBook-Pro.local>
2020-03-18 10:40:16 -0500

    before push

---
+++ butterfly-frontend/src/components/Show.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Show extends React.Component {
+  render () {
+
+    return (
+      <div>
+        <div className="modal">
+         <h3>{this.props.post.title.toUpperCase()}</h3>
+         <hr/>
+         {
+           this.props.post.image? <img src={this.props.post.image} alt = 'Post'></img> : ''
+         }
+
+         {
+           this.props.post.video? <iframe width="560" height="315" src={`${this.props.post.video}?autoplay=1`} frameBorder="0"></iframe>: ''
+         }
+         <h4>{this.props.post.caption}</h4>
+         <h6><span>Likes:</span> {this.props.post.likes}</h6>
+         {/* <p><span>comments:</span> { this.props.holiday.description } </p> */}
+       </div>
+     </div>
+    )
+  }
+ }
+export default Show
---
+++ butterfly-frontend/src/components/Signup.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react'
+class Signup extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      signup: true
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggleSignup = this.toggleSignup.bind(this)
+  }
+
+  toggleSignup(){
+    this.setState({
+      signup: !this.state.signup
+    })
+  }
+  handleChange(event){
+    this.setState({
+      [event.currentTarget.id]: event.currentTarget.value
+    })
+  }
+  async handleSubmit(event){
+    event.preventDefault()
+    try{
+      let response = await fetch(this.props.baseURL + '/users',{
+        method:'POST',
+        body: JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      let data = await response.json()
+      // console.log(data)
+      this.setState({
+        username:'',
+        password:'',
+        signup:true
+      })
+    }catch(e){
+      console.error(e);
+    }
+  }
+  render(){
+    return(
+      <div>
+      {
+          this.state.signup
+          ?
+          <button onClick={this.toggleSignup}>Sign up</button>
+          :
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="username">Userame</label>
+            <input type="text" id="username" name="username" onChange={this.handleChange} value={this.state.username} placeholder="username"/>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="password"/>
+            <input type='submit' value='sign up'/>
+          </form>
+        }
+
+      </div>
+    )
+  }
+}
+export default Signup
